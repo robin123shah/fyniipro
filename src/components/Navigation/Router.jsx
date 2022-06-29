4cef26d1dbd641eac093955b18e5fec1912fe8d0
Robin Shah <robin123shah@gmail.com>
2022-06-29 13:51:37 +0530

    1234323

--- src/components/Navigation/Router.jsx
+++ src/components/Navigation/Router.jsx
@@ -11,7 +11,7 @@ import SignIn from "../Screens/SignIn/SignIn"
 import Texteditor from '../Screens/TextEditor/Texteditor';
 import CoursePage from '../Screens/CoursePage/CoursePage';
 import Profile from '../Screens/Profile/Profile';
-import ButtonPage from '../Screens/Internship/Modal/ButtonPage';
+// import ButtonPage from '../Screens/Internship/Modal/ButtonPage';
 // import About from "../Screens/About/About"
 const Router = () => {
   return (
@@ -30,7 +30,7 @@ const Router = () => {
         <Route exact path='/texteditor' element={<Texteditor/>}/>
         <Route exact path='/CoursePage' element={<CoursePage/>}/>
         <Route exact path='/Profile' element={<Profile/>}/>
-        <Route exact path='/Modal' element={<ButtonPage/>}/>
+        {/* <Route exact path='/Modal' element={<ButtonPage/>}/> */}
         {/* <Route exact path='/about' element={<About/>}/> */}
         {/* {localStorage.getItem("login") === "true" && <Route path = '/*' element= {<Navbar/>}/>} */}
     </Routes>
