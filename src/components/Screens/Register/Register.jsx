45be6c80a2c55760dbb04ddec73f926ebc48a08f
prashant kumar <ojhaajit2612@gmail.com>
2022-09-26 16:07:58 +0530

    test

--- src/components/Screens/Register/Register.jsx
+++ src/components/Screens/Register/Register.jsx
@@ -84,7 +84,7 @@ const Register = () => {
       <div className="register-left">
         <form onSubmit={handleSubmit}>
         <h2 className="Register_h2">
-          Create Your Profiles
+          Create Your Profile
         </h2>
         {/* <div className="google"><GoogleButton onClick={onGoogleRegister} className="google-button"/></div> */}
 
